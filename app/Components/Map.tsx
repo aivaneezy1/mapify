7248559255680645fb3635cad4526cee73cda40e
aivaneezy1 <aivaneezydakii@gmail.com>
2024-08-18 03:18:59 +0200

    added a current positon marker and changed the icons

--- app/Components/Map.tsx
+++ app/Components/Map.tsx
@@ -17,8 +17,10 @@ const MapComponent = ({ places }: MapProps) => {
 
   const mapRef = useRef<HTMLDivElement>(null);
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const [marker, setMarker] =
-    useState<google.maps.marker.AdvancedMarkerElement | null>(null);
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+  const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow | null>(
+    null
+  );
 
   useEffect(() => {
     const initMap = async () => {
@@ -48,11 +50,28 @@ const MapComponent = ({ places }: MapProps) => {
       const mapInstance = new Map(mapRef.current as HTMLDivElement, options);
       setMap(mapInstance);
 
-      let markerInstance = new AdvancedMarkerElement({
+      let markerInstance = new google.maps.Marker({
         map: mapInstance,
         position: defaultLocation,
+        icon: {
+          url: "https://aivan-image.s3.eu-north-1.amazonaws.com/mapImage/man.png", 
+           scaledSize: new google.maps.Size(50, 50),
+        },
       });
       setMarker(markerInstance);
+
+      const infoWindowInstance = new google.maps.InfoWindow({
+        content:
+          "<div style='font-size: 16px; color: black;'>Current Position</div>",
+      });
+      setInfoWindow(infoWindowInstance);
+
+      // Show the info window above the marker
+      infoWindowInstance.open({
+        anchor: markerInstance,
+        map: mapInstance,
+        shouldFocus: false,
+      });
 
       mapInstance.addListener("click", (event: google.maps.MapMouseEvent) => {
         if (event.latLng) {
@@ -60,8 +79,15 @@ const MapComponent = ({ places }: MapProps) => {
             lat: event.latLng.lat(),
             lng: event.latLng.lng(),
           };
-          markerInstance.position = clickedLocation;
+          markerInstance.setPosition(clickedLocation);
           setCoordinates(clickedLocation);
+
+          // Update the info window position
+          infoWindowInstance.open({
+            anchor: markerInstance,
+            map: mapInstance,
+            shouldFocus: false,
+          });
         }
       });
 
@@ -75,8 +101,15 @@ const MapComponent = ({ places }: MapProps) => {
 
             mapInstance.setCenter(userLocation);
             mapInstance.setZoom(15);
-            markerInstance.position = userLocation;
+            markerInstance.setPosition(userLocation);
             setCoordinates(userLocation);
+
+            // Update the info window position for the new marker position
+            infoWindowInstance.open({
+              anchor: markerInstance,
+              map: mapInstance,
+              shouldFocus: false,
+            });
           },
           () => {
             console.error("User denied Geolocation");
@@ -109,7 +142,14 @@ const MapComponent = ({ places }: MapProps) => {
             lng: newLocation.lng,
           });
 
-          markerInstance.position = newLocation;
+          markerInstance.setPosition(newLocation);
+
+          // Update the info window position
+          infoWindowInstance.open({
+            anchor: markerInstance,
+            map: mapInstance,
+            shouldFocus: false,
+          });
         }
       });
     };
@@ -117,16 +157,15 @@ const MapComponent = ({ places }: MapProps) => {
     initMap();
   }, []);
 
-  // Geocode the addresses and place markers on the map
   const geocodeAddresses = (
-    addresses: string[], 
+    places: PlaceType[],
     mapInstance: google.maps.Map
   ) => {
     const geocoder = new google.maps.Geocoder();
 
-    if (addresses && addresses.length > 0) {
-      addresses.forEach((address) => {
-        geocoder.geocode({ address: address }, (results, status) => {
+    if (places && places.length > 0) {
+      places.forEach((place, index) => {
+        geocoder.geocode({ address: place.address }, (results, status) => {
           if (
             status === google.maps.GeocoderStatus.OK &&
             results &&
@@ -134,9 +173,36 @@ const MapComponent = ({ places }: MapProps) => {
           ) {
             const location = results[0].geometry.location;
 
-            new google.maps.marker.AdvancedMarkerElement({
+            // Create a custom info window with the place details
+            const infoWindowContent = `
+             <div style="display: flex; flex-direction: column; align-items: center; max-width: 250px; background-color: #f9f9f9; padding: 15px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+  <h3 style="font-size: 1.2em; color: #333; margin-bottom: 10px; text-align: center; font-weight:bold;">${place.name}</h3>
+
+  <img src="${place.pictures[0]}" alt="Restaurant Image" style="width: 80%; height: auto; object-fit: cover; border-radius: 10px; margin-bottom: 10px; border: 1px solid #ddd;" />
+
+  <p style="font-size: 1em; color: #666; margin: 0;">Rating: 
+    <span style="color: #ff9800; font-weight: bold;">${place.rating}</span>
+  </p>
+</div>
+
+            `;
+
+            const infoWindow = new google.maps.InfoWindow({
+              content: infoWindowContent,
+            });
+
+            const marker = new google.maps.Marker({
               map: mapInstance,
               position: location,
+              icon: {
+                url: "https://aivan-image.s3.eu-north-1.amazonaws.com/mapImage/restaurant.png",
+                scaledSize: new google.maps.Size(40, 40), // Resize the icon to 40x40 pixels
+              },
+            });
+
+            // Add a click listener to open the info window when the marker is clicked
+            marker.addListener("click", () => {
+              infoWindow.open(mapInstance, marker);
             });
           } else {
             console.error("Geocode failed: " + status);
@@ -146,14 +212,11 @@ const MapComponent = ({ places }: MapProps) => {
     }
   };
 
-  // Watch for changes to places and update the markers
   useEffect(() => {
     if (places && map) {
-      const addresses = places.map((place) => place.address);
-      geocodeAddresses(addresses, map);
+      geocodeAddresses(places, map);
     }
   }, [places, map]);
-
 
   return (
     <div
